03717cdf3893ae172efc87c18793d53663e46780
Chip Kirchner <kirchner.chip@gmail.com>
2022-04-04 10:11:15 -0400

    section 5 complete

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import helloController
     from "./controllers/hello-controller.js";
 import userController   from "./controllers/users-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
 app.use(express.json());
 helloController(app);
 userController(app);
+tuitsController(app);
 app.listen(4000);
